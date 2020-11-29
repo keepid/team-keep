e6af423475f686cc8897ab911bfc994ed2b4c108
Connor Chong <chong.connor@gmail.com>
2020-11-28 20:41:37 -0600

    fixing issues page

--- src/components/Issue.tsx
+++ src/components/Issue.tsx
@@ -23,7 +23,7 @@ class AboutUs extends Component<{}, {}> {
                   {' '}
                   <br />
                   {' '}
-                  is a serious stuggle
+                  is a serious struggle
                   {' '}
                   <br />
                   {' '}
@@ -43,16 +43,13 @@ class AboutUs extends Component<{}, {}> {
           <div className="row viewport-height-50">
             <div className="col-md-11 custom-vertical-center mx-auto text-center">
               <div id="info">
-
                 <img
                   className="svg-purple mb-5"
                   src={RectangleSVG}
                   alt="rectangle"
                 />
-
                 <p className="hero-subtext text-grey">
                   Unsheltered homelessness has risen by 22% since 2015, and the recent pandemic has only exacerbated this problem.
-
                 </p>
                 <p className="hero-subtext text-black ">
                   Lacking identification documents perpetuates this cycle of homelessness by barring access to essential services. It is a chronic problem that needs to be addressed.
@@ -64,78 +61,52 @@ class AboutUs extends Component<{}, {}> {
 
         <div className="bordered-background1 viewport-height-100">
           <div className="container my-auto text-center">
-            <div className="custom-vertical-center align-items-center margin-left margin-top1 align-center">
-              <div>
-                <div className="mx-auto ">
-                  <h3 className="hero-header1 font-weight-bold pb-3 margin-top1">
+            <div className="py-5 my-4 custom-vertical-center align-items-center align-center">
+              <div className="row">
+                <div className="mx-auto col-md-12">
+                  <h3 className="hero-header1 font-weight-bold pb-4">
                     Homelessness in the US
                   </h3>
-                  <div>
-                    {' '}
-                    <br />
-                  </div>
-                  <p className="text-purple margin-top2">
+                </div>
+                <div className="mx-auto col-md-8">
+                  <p className="text-purple">
                     552,820 people
                   </p>
-
                   <p className="text-light-black home-container-text">
                     The size of the population experiencing
-                    {' '}
-                    <br />
-                    {' '}
                     homelessness on a single night in 2019
                   </p>
-
-                  <div>
-                    {' '}
-                    <br />
-                  </div>
                   <p className="text-purple ">
                     5,800 people
                   </p>
-
                   <p className="text-light-black home-container-text">
                     The number of chronically homeless
-                    {' '}
-                    <br />
-                    {' '}
                     individuals in Philadelphia
                   </p>
-
-                  <div>
-                    {' '}
-                    <br />
-                  </div>
                   <p className="text-purple ">
                     6,583 people
                   </p>
                   <p className="text-light-black home-container-text">
                     The number of youth experiencing
-                    {' '}
-                    <br />
-                    {' '}
                     homelessness in Philadelphia
                   </p>
-
-                  <p className="text-purple1 margin-top2">
+                  <p className="text-purple1">
                     Source: Annual Homelessness Assessment Report of 2019
                   </p>
                 </div>
-
               </div>
             </div>
           </div>
         </div>
-
-        <div className="bordered-background2">
-          <div className=" my-auto text-center miniborder-background">
-            <div className=" container custom-vertical-center align-items-center ">
-              <div className="margin-top1">
+        <div className="bordered-background2 ">
+          <div className=" my-auto text-center miniborder-background ">
+            <div className=" container custom-vertical-center align-items-center viewport-height-100">
+              <div className="py-5 my-  3">
                 <div className="mx-auto">
-                  <h3 className="hero-header2 font-weight-bold pb-3 ">
-                    What happens when you dont have identification?
+                  <h3 className="hero-header2 font-weight-bold pb-3">
+                    What happens when you don&apos;t have identification?
                   </h3>
-                  <div className="row margin-top1">
+                  <div className="row my-4">
                     <div className="col-md-3 mx-auto hero-header2 ">
                       54%
                     </div>
@@ -149,7 +120,7 @@ class AboutUs extends Component<{}, {}> {
                     </div>
                   </div>
 
-                  <div className="row margin-top1">
+                  <div className="row my-4">
                     <div className="col-md-3 mx-auto hero-header2 ">
                       53%
                     </div>
@@ -164,7 +135,7 @@ class AboutUs extends Component<{}, {}> {
                     </div>
                   </div>
 
-                  <div className="row margin-top1">
+                  <div className="row my-4">
                     <div className="col-md-3 mx-auto hero-header2 ">
                       45%
                     </div>
@@ -178,7 +149,7 @@ class AboutUs extends Component<{}, {}> {
                       </p>
                     </div>
                   </div>
-                  <p className="text-purple1 margin-top1">
+                  <p className="text-purple1">
                     Source: Annual Homelessness Assessment Report of 2019
                   </p>
                 </div>
@@ -216,7 +187,7 @@ class AboutUs extends Component<{}, {}> {
                   We are here to help!
                 </h1>
                 <a href="https://teamkeep.nationbuilder.com/donate">
-                  <button type="button" className="btn btn-lg btn-secondary margin-top1">
+                  <button type="button" className="btn btn-lg btn-secondary">
                     Donate Now
                   </button>
                 </a>
