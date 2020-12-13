ed4c64d3b2ad327efc43414b075b1181263645b5
Melinda Cardenas <mc6377@nyu.edu>
2020-12-13 02:08:52 -0500

    created holiday gifting page skeleton

---
+++ src/components/HolidayGiftingExchange.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { withAlert } from 'react-alert';
+
+class HolidayGiftingExchange extends Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
+
+export default withAlert()(HolidayGiftingExchange);
