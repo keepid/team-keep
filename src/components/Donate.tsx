497317cfb3138636be911349f068ec29bba76876
daniel joo <dsj2122@columbia.edu>
2021-06-30 20:47:09 -0700

    implemented UI skeleton

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -4,7 +4,12 @@ import { withAlert } from 'react-alert';
 
 // image imports
 
-class Donate extends Component<{}, {}> {
+class Donate extends Component<{}, {amount: number}> {
+  constructor(props) {
+    super(props);
+    this.state = { amount: 0 };
+  }
+
   render() {
     return (
       <div>
@@ -12,8 +17,17 @@ class Donate extends Component<{}, {}> {
           <title>Welcome</title>
           <meta name="description" content="Keep.id" />
         </Helmet>
-        <a>Hello!</a>
-
+        <header className="text-left m-2">Hello! Click a button or put in your own amount</header>
+        <div className="btn-group" role="group">
+          <button className="btn btn-primary m-1" type="button" onClick={() => this.setState({ amount: 5 })}>5</button>
+          <button className="btn btn-primary m-1" type="button" onClick={() => this.setState({ amount: 10 })}>10</button>
+          <button className="btn btn-primary m-1" type="button" onClick={() => this.setState({ amount: 15 })}>15</button>
+          <button className="btn btn-primary m-1" type="button" onClick={() => this.setState({ amount: 20 })}>20</button>
+          <div className="input-group m-1">
+            <input type="text" className="form-control" value={this.state.amount} />
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Donate!</button>
+          </div>
+        </div>
       </div>
     );
   }
