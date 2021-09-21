33aa56df79f1b802a0b8d29478c850bb77eb7bd9
ykl1 <lyoungkwang.yl@gmail.com>
2021-09-20 23:45:56 -0400

    add stories boilerplate

---
+++ src/components/Stories.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+// TODOS:
+// create a simple card
+// get access to team keep medium account
+// create access token to use medium api
+// make a simple request to get article metadata
+// display the metadata via the card
+// implement the request and card display for all medium articles in a useEffect call
+
+const Stories = () => (
+  <div>
+    <h1>Story</h1>
+  </div>
+);
+
+export default Stories;
