13422e5d343636fc7a127c956dbf3b1a45c62693
ykl1 <lyoungkwang.yl@gmail.com>
2021-10-11 00:29:51 -0400

    additional formating to cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,17 +4,22 @@ const formatTitle = (title) => title.replace(/:|amp;/g, '');
 
 const formatDescription = (description) => {
   const descriptionWithoutHtmlTags = description.replace(/(<([^>]+)>)/ig, '');
-  const shortedDescription = descriptionWithoutHtmlTags.split(' ').splice(0, 18).join(' ').trim();
-  return `"${shortedDescription}..."`;
+  const shorteneddDescription = descriptionWithoutHtmlTags.split(' ').splice(0, 18).join(' ').trim();
+  return `"${shorteneddDescription}..."`;
 };
 
-const Card = ({ imgUrl, title, url, description }) => (
+const Card = ({
+  imgUrl, title, url, description,
+}) => (
   <div className="grid-item">
     <div className="card">
       <img className="card-img" src={imgUrl} alt="Medium Article" />
       <div className="card-content">
         <h1 className="card-header">{`${formatTitle(title)}`}</h1>
         <p className="card-text">{`${formatDescription(description)}`}</p>
+        <a href={url} target="_blank" rel="noreferrer">
+          <button type="button" className="card-btn">Read More</button>
+        </a>
       </div>
     </div>
   </div>
--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Helmet } from 'react-helmet';
 import Card from './Card';
+import RectangleSVG from '../static/images/rectangle.svg';
 
 const url = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/keep-informed';
 
@@ -14,16 +16,47 @@ const Stories = () => {
   }, []);
   return (
     <div>
-      <h1>A collection of writing by Team Keep about the homeless experience.</h1>
+      <Helmet>
+        <title>Stories</title>
+        <meta name="description" content="Keep.id" />
+      </Helmet>
+      <div className="bg-story-hero-img viewport-height-100 negative-margin-top">
+        <div className="container">
+          <div className="viewport-height-100 custom-vertical-center">
+            <div>
+              <h1 className="hero-header pb-4 text-white">
+                Keep.Informed
+              </h1>
+              <p className="hero-header-2 text-white">
+                <img
+                  className="svg-white mb-5"
+                  src={RectangleSVG}
+                  alt="rectangle"
+                />
+                <br />
+                A collection of writing by Team
+                {' '}
+                <br />
+                {' '}
+                Keep about the homeless
+                {' '}
+                <br />
+                {' '}
+                experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="stories-sizing stories-body">
         <div className="grid">
           {mediumData.map((article) => (
             <Card
-              key={article['guid']}
-              imgUrl={article['thumbnail']}
-              title={article['title']}
-              url={article['link']}
-              description={article['description']}
+              key={article.guid}
+              imgUrl={article.thumbnail}
+              title={article.title}
+              url={article.link}
+              description={article.description}
             />
           ))}
         </div>
