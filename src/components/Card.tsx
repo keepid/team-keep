a4dae4a2532a1b7124557e77c67bfa89ff98227a
ssung01-pixel <ssung101@bu.edu>
2022-03-01 22:50:23 -0500

    Fix learn more button drop down

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
 const formatTitle = (title) => title.replace(/:|amp;/g, '');
 
 const formatDescription = (description) => {
-  const descriptionWithoutHtmlTags = description.replace(/(<([^>]+)>)/ig, '');
-  const shorteneddDescription = descriptionWithoutHtmlTags.split(' ').splice(0, 18).join(' ').trim();
+  const descriptionWithoutHtmlTags = description.replace(/(<([^>]+)>)/gi, '');
+  const shorteneddDescription = descriptionWithoutHtmlTags
+    .split(' ')
+    .splice(0, 18)
+    .join(' ')
+    .trim();
   return `"${shorteneddDescription}..."`;
 };
 
@@ -17,10 +22,19 @@ const Card = ({
       <img className="card-img" src={imgUrl} alt="Medium Article" />
       <div className="card-content">
         <h1 className="card-header">{`${formatTitle(title)}`}</h1>
-        <p className="card-date-text">{`${formatTitle(moment(date).format('MMM. Do, YYYY'))}`}</p>
+        <p className="card-date-text">
+          {`${formatTitle(moment(date).format('MMM. Do, YYYY'))}`}
+        </p>
         <p className="card-text">{`${formatDescription(description)}`}</p>
-        <a href={url} style={{ textDecoration: 'none' }} target="_blank" rel="noreferrer">
-          <button type="button" className="card-btn">Read More</button>
+        <a
+          href={url}
+          style={{ textDecoration: 'none' }}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button" className="card-btn">
+            Read More
+          </button>
         </a>
       </div>
     </div>
@@ -28,3 +42,18 @@ const Card = ({
 );
 
 export default Card;
+Card.defaultProps = {
+  imgUrl: 'This has no image',
+  title: 'This has no title',
+  url: 'This has no url',
+  description: 'This has no description',
+  date: 'This has no date',
+};
+
+Card.propTypes = {
+  imgUrl: PropTypes.string,
+  title: PropTypes.string,
+  url: PropTypes.string,
+  description: PropTypes.string,
+  date: PropTypes.string,
+};
